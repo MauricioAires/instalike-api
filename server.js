094a868b5dc562c5b7ecf0572c4d3040bbcd6990
MauricioAires <mauricioaires015@gmail.com>
2024-11-21 14:24:23 -0300

    feat: create route find by id

--- server.js
+++ server.js
@@ -1,13 +1,62 @@
 import express from 'express';
 
+// mock / array em memória
+const posts = [
+  {
+    id: 1,
+    descricao: 'Uma foto testes',
+    imagem: 'https://placecats.com/300/200'
+  },
+  {
+    id: 2,
+    descricao: 'Paisagem montanhosa',
+    imagem: 'https://placecats.com/300/200'
+  },
+  {
+    id: 3,
+    descricao: 'Cachorro brincando',
+    imagem: 'https://placecats.com/300/200'
+  },
+  {
+    id: 4,
+    descricao: 'Citação inspiradora',
+    imagem: 'https://placecats.com/300/200'
+  },
+  {
+    id: 5,
+    descricao: 'Comida deliciosa',
+    imagem: 'https://placecats.com/300/200'
+  },
+  {
+    id: 6,
+    descricao: 'Cidade à noite',
+    imagem: 'https://placecats.com/300/200'
+  }
+];
+
+
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
-app.get('/api', (req, res) => {
-  res.status(200).send('Boas vindas à imersão!');
+app.get('/posts', (req, res) => {
+  res.status(200).json(posts);
+});
+
+
+function buscarPostPorID(id){
+  return posts.find(post => post.id === parseInt(id));
+}
+
+app.get('/posts/:id', (req, res) => {
+
+  const post = buscarPostPorID(req.params.id);
+
+  res.status(200).json(post);
 });
 
 
@@ -16,4 +65,15 @@ app.get('/api', (req, res) => {
  * To ask good questions to an IA, pass contexts
  * - Eu estou estudando, // I am studying
  * - Eu quero ganhar dinheiro, // I want to make money
+ */
+
+/**
+ *  Site com exemplos de códigos HTTP:
+ *  @see https://http.cat/
+ */
+
+/**
+ * Site com imagens de gatos para teste:
+ *
+ * @see https://placecats.com/300/200
  */
